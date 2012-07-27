1da0e344f0f8f80c2e78965f9b27adba54f20a29
Philippe Lavoie <drdeteck@gmail.com>
2012-07-26 23:31:44 -0400

    Rajout du Loading
    Update a la version 1.2.pre de jquery mobile

--- HoublonVa.Service/Scripts/script.js
+++ HoublonVa.Service/Scripts/script.js
@@ -4,6 +4,12 @@ $(document).ready(initialization);
 
 function initialization() {
 
+    $.mobile.loading('show', {
+        text: 'Recherche de la position...',
+        textVisible: true,
+        html: ""
+    });
+
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
             getPlaces,
@@ -17,20 +23,27 @@ function initialization() {
 }
 
 function getPlaces(position) {
+    $.mobile.loading('show', {
+        text: 'Chargement des lieux ...',
+        textVisible: true,
+        html: ""
+    });
+
     $.ajax(
     { type: 'GET',
         url: '/API/Place.svc/GetNearestPlace?lng=' + position.coords.longitude +'&lat=' + position.coords.latitude + '&qt=' + quantity,
         dataType: 'text',
         success: function (data) {
-            console.log(data);
             var places = JSON.parse(data);
             places = JSON.parse(places["GetNearestPlaceResult"]);
 
             $.each(places, appendPlace);
 
             $("#places-list").listview("refresh");
+            $.mobile.loading('hide');
         },
         error: function (jqXHR, textStatus, errorThrown) {
+            $.mobile.loading('hide');
             console.log("error " + textStatus);
             console.log("incoming Text " + jqXHR.responseText);
         }
@@ -38,12 +51,14 @@ function getPlaces(position) {
 }
 
 function appendPlace(index, value) {
-    console.log(index);
-    console.log(value);
+    var distance = '<span class="ui-li-count">' + value["distance"].toFixed(2) + ' km</span>';
+    var image = "<img style='height:16px; width:16px;' class='ui-li-icon' src='" + value["icon"] + "'/>";
+    var title = "<h3 class='ui-li-heading'>" + formatName(value["name"]) + "</h3>";
+    var description = "<p class='ui-li-desc'>" + value["formatted_address"] + "</p>";
+
     $("#places-list").append(
         $('<li>').append(
-            $("<a>").attr("href", value["url"]).append(formatName(value["name"]) + '<span class="ui-li-count">' + value["distance"].toFixed(2) + ' km</span>'
-            + "<img style='height:16px; width:16px;' class='ui-li-icon' src='" + value["icon"] + "'/>")
+            $("<a>").attr("href", value["url"]).append(title + description + distance + image)
     ));
 }
 
