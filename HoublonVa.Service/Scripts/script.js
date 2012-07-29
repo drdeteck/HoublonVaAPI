36c9b854afbcec768a4090a46fbd42ffb5b0b927
Philippe Lavoie <drdeteck@gmail.com>
2012-07-28 22:51:27 -0400

    Add page and filter

--- HoublonVa.Service/Scripts/script.js
+++ HoublonVa.Service/Scripts/script.js
@@ -1,81 +1,163 @@
-var quantity = 0;
+// iO namespace root
+window.HV = window.HV || {};
 
-$(document).ready(initialization);
+// ====================
+// GeoPlace module
+// ====================
+(function (Utilities, $, undefined) {
 
-function initialization() {
+    // Public Method
 
-    $.mobile.loading('show', {
-        text: 'Recherche de la position...',
-        textVisible: true,
-        html: ""
-    });
+} (HV.Utilities = HV.Utilities || {}, $));
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-            getPlaces,
-            errorNoLocation,
-            { enableHighAccuracy: true, timeout: 10000, maximumAge: 0 }
-        );
+// ====================
+// GeoPlace module
+// ====================
+(function (UI, $, undefined) {
+    
+    UI.initialization = function () {
+        
+        $('#checkbox-1').click(function () {
+            HV.Filter.toggle("hv-filter-micro");
+        });
+        $('#checkbox-2').click(function () {
+            HV.Filter.toggle("hv-filter-seller");
+        });
+        $('#checkbox-3').click(function () {
+            HV.Filter.toggle("hv-filter-pub");
+        });
     }
-    else {
-        errorNoLocation();
-    }
-}
 
-function getPlaces(position) {
-    $.mobile.loading('show', {
-        text: 'Chargement des lieux ...',
-        textVisible: true,
-        html: ""
-    });
-
-    $.ajax(
-    { type: 'GET',
-        url: '/API/Place.svc/GetNearestPlace?lng=' + position.coords.longitude +'&lat=' + position.coords.latitude + '&qt=' + quantity,
-        dataType: 'text',
-        success: function (data) {
-            var places = JSON.parse(data);
-            places = JSON.parse(places["GetNearestPlaceResult"]);
-
-            $.each(places, appendPlace);
-
-            $("#places-list").listview("refresh");
-            $.mobile.loading('hide');
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            $.mobile.loading('hide');
-            console.log("error " + textStatus);
-            console.log("incoming Text " + jqXHR.responseText);
+    // Public Method
+    UI.Loading = function (doShow, message) {
+        if (doShow) {
+            $.mobile.loading('show', {
+                text: message,
+                textVisible: true,
+                html: ""
+            });
         }
-    });
-}
+        else {
+             $.mobile.loading('hide');
+        }
+    };
 
-function appendPlace(index, value) {
-    var distance = '<span class="ui-li-count">' + value["distance"].toFixed(2) + ' km</span>';
-    var image = "<img style='height:16px; width:16px;' class='ui-li-icon' src='" + value["icon"] + "'/>";
-    var title = "<h3 class='ui-li-heading'>" + formatName(value["name"]) + "</h3>";
-    var description = "<p class='ui-li-desc'>" + value["formatted_address"] + "</p>";
+    UI.ShowNavBar = function () {
+        $('#hv-navbar').show();
+    }
+    
 
-    $("#places-list").append(
-        $('<li>').append(
-            $("<a>").attr("href", value["url"]).append(title + description + distance + image)
-    ));
-}
+} (HV.UI = HV.UI || {}, $));
 
-// Remove the parenthesis and put the article in front
-function formatName(name) {
-    var start = name.indexOf("(");
-    var end = name.indexOf(")");
 
-    if (start >= 0 && end > 0 && start < end) {
-        var subWord = name.substr(start, end - start + 1);
-        var prefix = subWord.substr(1, subWord.length - 2);
-        name = prefix + " " + name.replace(subWord, "");
+// ====================
+// GeoPlace module
+// ====================
+(function (GeoPlace, $, undefined) {
+    // Private properties
+
+    // Public properties
+    GeoPlace.quantity = 0;
+
+    // Public Methods
+
+    GeoPlace.initialization = function () {
+
+        HV.UI.Loading(true, 'Recherche de la position...');
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                getPlaces,
+                errorNoLocation,
+                { enableHighAccuracy: true, timeout: 10000, maximumAge: 0 }
+            );
+        }
+        else {
+            errorNoLocation();
+        }
+        
+        HV.UI.initialization();
+    };
+
+    // Private Methods
+
+    function getPlaces(position) {
+
+        HV.UI.Loading(true, 'Chargement des lieux ...');
+
+        $.ajax(
+        { type: 'GET',
+            url: '/API/Place.svc/GetNearestPlace?lng=' + position.coords.longitude +'&lat=' + position.coords.latitude + '&qt=' + HV.GeoPlace.quantity,
+            dataType: 'text',
+            success: function (data) {
+                var places = JSON.parse(data);
+                places = JSON.parse(places["GetNearestPlaceResult"]);
+
+                // Append each Place to the Places List
+                $.each(places, appendPlace);
+
+                // Refresh the list and hide the loading
+                $("#hv-places-list").listview("refresh");
+                HV.UI.Loading(false);
+                HV.UI.ShowNavBar();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $.mobile.loading('hide');
+                console.log("error " + textStatus);
+                console.log("incoming Text " + jqXHR.responseText);
+            }
+        });
     }
 
-    return name;
-}
+    // Method to Append each place to the Places List
+    function appendPlace(index, value) {
 
-function errorNoLocation() {
+        // Prepare each section
+        var colorClass = "hv-filter-micro";
+        if (value["category"] == 3) {
+            colorClass = "hv-filter-seller";
+        }
 
-}
+        var distance = '<span class="ui-li-count">' + value["distance"].toFixed(2) + ' km</span>';
+        var image = "<img style='height:16px; width:16px;' class='ui-li-icon' alt='micro' src='" + value["icon"] + "'/>";
+        var title = "<h3 class='ui-li-heading " + colorClass + "'>" + formatName(value["name"]) + "</h3>";
+        var description = "<p class='ui-li-desc'>" + value["formatted_address"] + "</p>";
+
+        // Append the List item 
+        $("#hv-places-list").append(
+            $('<li>').append(
+                $("<a>").attr("href", value["url"]).append(title + description + distance + image)
+        ));
+    }
+
+    // Remove the parenthesis and put the article in front
+    function formatName(name) {
+        var start = name.indexOf("(");
+        var end = name.indexOf(")");
+
+        if (start >= 0 && end > 0 && start < end) {
+            var subWord = name.substr(start, end - start + 1);
+            var prefix = subWord.substr(1, subWord.length - 2);
+            name = prefix + " " + name.replace(subWord, "");
+        }
+
+        return name;
+    }
+
+    // Error callback if there is no navigation geo location
+    function errorNoLocation() {
+
+    }
+
+} (HV.GeoPlace = HV.GeoPlace || {}, $));
+
+// ====================
+// Filter module
+// ====================
+(function (Filter, $, undefined) {
+    
+    HV.Filter.toggle = function (categoryCssClass) {
+        $("#hv-places-list").find("li h3." + categoryCssClass).parents("li").toggleClass("ui-screen-hidden");
+    }
+
+} (HV.Filter = HV.Filter || {}, $));
